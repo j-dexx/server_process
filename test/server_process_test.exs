9263ed4c55a64152d059412b17200ff2fb16dd32
James Doyley <jdoyley@gmail.com>
2018-05-08 18:22:42 +0100

    Add Synchronous requests

---
+++ test/server_process_test.exs
@@ -0,0 +1,22 @@
+defmodule ServerProcessTest do
+  use ExUnit.Case
+  doctest ServerProcess
+
+  test "stores values" do
+    pid = KeyValueStore.start()
+    KeyValueStore.put(pid, :a, 1)
+    KeyValueStore.put(pid, :b, 2)
+    assert KeyValueStore.get(pid, :a) == 1
+    assert KeyValueStore.get(pid, :b) == 2
+
+    Process.exit(pid, :kill)
+  end
+
+  test "values not stored returns nil" do
+    pid = KeyValueStore.start()
+
+    assert KeyValueStore.get(pid, :c) == nil
+
+    Process.exit(pid, :kill)
+  end
+end
